b26400832959a187d1125ef9442c89232447c59c
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-07-09 10:05:32 -0700

    updated manifest.json in public folder with site themes and name

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import Layout from './components/pages/layout';
 import './App.css';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { apiResponse: '' };
-	}
-	callAPI() {
-		fetch('http://localhost:9000/testAPI')
-			.then(res => res.text())
-			.then(res => this.setState({ apiResponse: res }));
-	}
 
-	componentDidMount() {
-		this.callAPI();
-	}
 
 	render() {
 		return (

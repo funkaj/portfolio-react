ae08e18334d81cb3bf5fd0279af55f261a3ce420
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-07-09 09:05:11 -0700

    build

---
+++ build/precache-manifest.1bbfe153a9beaa2a4045d90e0449d984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8619609e8a195108e0749619a85f4ba4",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "961b0e3753e1702c5d3e",
+    "url": "/portfolio-react/static/css/main.ae429801.chunk.css"
+  },
+  {
+    "revision": "63078df09c5788637dcd",
+    "url": "/portfolio-react/static/js/2.62510860.chunk.js"
+  },
+  {
+    "revision": "961b0e3753e1702c5d3e",
+    "url": "/portfolio-react/static/js/main.0ff5c373.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  }
+]);

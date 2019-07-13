4f04e2bc84f948b069557a43ccacc088d0882ea8
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-07-13 09:27:04 -0700

    began implementing react router as a component for use with sublinks docs here https://reacttraining.com/react-router/web/example/route-config

--- src/index.js
+++ src/index.js
@@ -2,20 +2,60 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./index.css";
-
 import Main from "./components/pages/main";
-
+import Gallery from "./components/pages/gallery";
 import Illustration from "./components/pages/Illustration";
 import WebDev from "./components/pages/web-development";
 import NotFound from "./components/pages/404";
 import * as serviceWorker from "./serviceWorker";
 
+const routes = [
+  {
+    path: "/",
+    component: Main
+  },
+  {
+    path: "/web-development/",
+    component: WebDev
+  },
+  {
+    path: "/gallery/",
+    component: Gallery,
+    routes: [
+      {
+        path: "/gallery/illustration/",
+        component: Illustration
+      },
+      {
+        path: "/gallery/web-development/",
+        component: WebDev
+      }
+    ]
+  }
+];
+
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
+
 const routing = (
   <Router>
     <Switch>
-      <Route exact path="/" component={Main} />
+      {routes.map((route, i) => (
+        <RouteWithSubRoutes key={i} {...route} />
+      ))}
+      {/* <Route exact path="/" component={Main} />
+      <Route path="/gallery/" component={Gallery} />
       <Route path="/illustration/" component={Illustration} />
-      <Route path="/web-development/" component={WebDev} />
+      <Route path="/web-development/" component={WebDev} /> */}
       <Route component={NotFound} />
     </Switch>
   </Router>

aec41ca64ea423c6dcb438cabf7282cd5a02d35c
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-07-19 14:34:19 -0700

    update route

--- build/precache-manifest.3d3825762f33ebb29a559e1163c4dd06.js
+++ build/precache-manifest.3d3825762f33ebb29a559e1163c4dd06.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2bca267f5059508d1e8b352e9fc56857",
+    "revision": "bff3a5e9b6f4776e1cefea0281b0ecdb",
     "url": "/index.html"
   },
   {
-    "revision": "8f17c8dc0cec73507990",
+    "revision": "62d3b15ea55d37b27847",
     "url": "/static/css/main.ac9897ad.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.35efd37a.chunk.js"
   },
   {
-    "revision": "8f17c8dc0cec73507990",
-    "url": "/static/js/main.f4d8f2cb.chunk.js"
+    "revision": "62d3b15ea55d37b27847",
+    "url": "/static/js/main.7e779233.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",

fe208678de8b86ae45b4cb9be9f0d63e4469a50f
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-07-26 13:02:12 -0700

    Updates

---
+++ precache-manifest.ee97a70097ff74a83b38e683a4a8afe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6af963ded7989c701513f4e8e5613c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001bdbb1d5bb81b24915",
+    "url": "/static/css/main.10b3c8de.chunk.css"
+  },
+  {
+    "revision": "9c2d27b23b2a6efd562f",
+    "url": "/static/js/2.31474f16.chunk.js"
+  },
+  {
+    "revision": "001bdbb1d5bb81b24915",
+    "url": "/static/js/main.af7de6d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

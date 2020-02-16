52c8a197f25b1b9a9f80d014dac12ef3be480c5b
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2020-02-16 09:05:27 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c75570bd6e45f06ae9f8d324cfca47b1.js"
+  "/precache-manifest.ee6e6a3fea84a63b94811a71cab9a341.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
